dae528a983061a54395aa220376e1fda44e0a96c
Maxime <maximeletourneau.1@gmail.com>
2017-12-14 21:39:31 -0500

    fixing ssh

--- compose/jenkins/data/deploy.sh
+++ compose/jenkins/data/deploy.sh
@@ -8,7 +8,9 @@ rm -rf vaolo
 git clone git://github.com/MaximeLet/vaolo.git
 cd vaolo
 git remote set-url origin git@github.com:MaximeLet/vaolo.git
+git checkout -b test origin/test
+git merge origin/master
+git push --all
 git checkout -b staging origin/staging
 git merge origin/test
 git push --all
-
--- compose/test_server/test.sh
+++ compose/test_server/test.sh
@@ -2,14 +2,15 @@
 #
 # Automates testing of current project following changes in the master branch
 #
-ssh -T git@github.com
+#ssh -T git@github.com
 rm -rf vaolo
-git clone git://github.com/MaximeLet/vaolo.git
+#git clone git://github.com/MaximeLet/vaolo.git
+git clone https://github.com/MaximeLet/vaolo.git
 cd vaolo
-git remote set-url origin git@github.com:MaximeLet/vaolo.git
+#git remote set-url origin git@github.com:MaximeLet/vaolo.git
 git checkout -b test origin/test
 git merge master
-git push --all
+#git push --all
 npm install
 npm run test-ci
 
